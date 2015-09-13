2bbcb5aa9873ddf3f9a780790ebf0f954aaad9a5
Zack <abc@0el.xyz>
2015-09-14 01:02:04 +0800

    hello world

---
+++ src/app.vue
@@ -0,0 +1,29 @@
+<template lang="jade">
+    div.container
+        header
+</template>
+
+<style lang="stylus">
+    html, body
+        margin: 0 0
+        padding: 0 0
+        border: 0
+        font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+        font-size: 14px
+
+    .container
+        width: 960px
+        margin: 0 auto
+</style>
+
+<script type="babel">
+    import header from './components/header.vue';
+    export default {
+        data() {
+            return {title : "hello world"};
+        },
+        components: {
+            'header' : header
+        }
+    };
+</script>
---
+++ src/index.vue
@@ -0,0 +1,15 @@
+<template lang="jade">
+    nav
+</template>
+
+<script type="babel">
+    import nav from './components/nav.vue';
+    export default {
+        data() {
+            return {title : "hello world"};
+        },
+        components: {
+            'nav' : nav
+        }
+    };
+</script>
